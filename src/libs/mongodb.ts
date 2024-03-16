077b46cf20067a1e87d734cddf2ab39a205cb261
Papucho5324 <alfonsochavez_jrz@hotmail.com>
2024-03-15 18:18:38 -0600

    coneccion con la bd y registro

--- src/libs/mongodb.ts
+++ src/libs/mongodb.ts
@@ -1,22 +1,20 @@
 import mongoose from "mongoose";
 
-const {MONGODB_URI} = process.env
+const { MONGODB_URI } = process.env;
 
-if (!MONGODB_URI){
-    throw new Error ('MONGODB_URI must be defined');
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI must be defined");
 }
 
 export const connectDB = async () => {
-    try {
-        const {connection} = await mongoose.connect('process.env.MONGODB_URI');
-        if ( connection.readyState ==1){
-            console.log("MongoDB connected")
-            return Promise.resolve(true);
+  try {
+    const { connection } = await mongoose.connect(MONGODB_URI);
+    if (connection.readyState === 1) {
+      console.log("MongoDB Connected");
+      return Promise.resolve(true);
     }
-        
-    } catch (error) {
-        console.log(error);
-        return Promise.reject(false);
-        
-    }
+  } catch (error) {
+    console.error(error);
+    return Promise.reject(error);
+  }
 };
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,28 +1,32 @@
 import { Schema, model, models } from "mongoose";
 
-const UserSchema = new Schema({
-email: {
-    type: String,
-    unique: true,
-    requierd: [true, "Email is requiered"],
-    match: [
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email is required"],
+      match: [
         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
         "Email is invalid",
-    ],
-},
-password: {
-    type: String,
-    required: [true, "Password is requiered"],
-    select: false
-},
-fullname: {
-    type: String,
-    requied: [true, "Fullname is required"],
-    minLength: [3, "Fullname must be at least 3 characters"],
-    maxLength: [50, "Fullname must be at most 50 characters"]
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+    },
+    fullname: {
+      type: String,
+      required: [true, "fullname is required"],
+      minLength: [3, "fullname must be at least 3 characters"],
+      maxLength: [20, "fullname must be at most 20 characters"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-}  
-});
-
-const User =  models.User ||  model('User', UserSchema);
+const User = models.User || model("User", UserSchema);
 export default User;
