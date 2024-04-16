a7387741d4b5020916f2d3b4eca05f6644a3806a
Papucho5324 <alfonsochavez_jrz@hotmail.com>
2024-04-15 22:22:31 -0600

    modificacion en mongodb

--- src/libs/mongodb.ts
+++ src/libs/mongodb.ts
@@ -4,13 +4,15 @@ const { MONGODB_URI } = process.env;
 
 
 
-if (!MONGODB_URI) {
+//if (!MONGODB_URI) {
   throw new Error("MONGODB_URI must be defined");
-}
+//}
+
+const uri = "mongodb+srv://papucho:Alchu3101@cluster0.u3j4o7l.mongodb.net/xpectraWEB.usuarios"
 
 export const connectDB = async () => {
   try {
-    const { connection } = await mongoose.connect(MONGODB_URI);
+    const { connection } = await mongoose.connect(uri);
     if (connection.readyState === 1) {
       console.log("MongoDB Connected");
       return Promise.resolve(true);
