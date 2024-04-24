f697c5a0bfa5849ed51d6a5077914402f2fd7280
Papucho5324 <alfonsochavez_jrz@hotmail.com>
2024-04-23 21:47:17 -0600

    NEXTURL

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -44,8 +44,8 @@ const handler = NextAuth({
     async jwt({ token, user }) {
       if (user) token.user = user;
       return token;
-      
     },
+    
     
     async session({ session, token }) {
       session.user = token.user as any;
